02cb4ffdb382f3c22b8b80f8043eb74e86307c67
Aisha Yusuff <a.yusuff99@outlook.com>
2022-10-24 22:55:05 +0100

    Create student objects and save into db

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.MAY;
+
+@Configuration
+public class StudentConfig {
+
+//Params - allow it to access our db via StudentRepository class
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repo) {
+        return args -> {
+            Student aisha = new Student(
+                    "Aisha",
+                    "aisha.y@gmail.com",
+                    LocalDate.of(1999, MAY, 29),
+                    23
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(1997, MAY,4),
+                    25
+            );
+
+//            Save students into our db
+            repo.saveAll(
+                    List.of(aisha, alex));
+
+        };
+    }
+}
