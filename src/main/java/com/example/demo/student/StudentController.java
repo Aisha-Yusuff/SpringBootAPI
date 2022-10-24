0551ae473f45c7dfafd4a2075f50a58c75a7fb9b
Aisha Yusuff <a.yusuff99@outlook.com>
2022-10-24 22:01:07 +0100

    Connect to SQL db and use Spring JPA dependency

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
+//    Dependency Injection
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
