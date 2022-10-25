839419fee4186b9e1a9cb43d9e7dd1edd5212dfd
Aisha Yusuff <a.yusuff99@outlook.com>
2022-10-26 00:00:54 +0100

    Add email validation for new students

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // This interface is responsible for accessing the data in the db
 @Repository
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
+// Find student by email
+//    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
